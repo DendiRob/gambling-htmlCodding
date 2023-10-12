ffabb50601f7b827e2ae577fd49c233f3393ff1e
dendiroblek <dendiroblek@gmail.com>
2023-10-12 14:10:53 +0400

    added footer

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// //mainContent scroll scripts
+
+// const articlesContainer = document.querySelector('.mainContent__blog_articles-container');
+// const articles = document.querySelector('.mainContent__blog_articles');
+// let startY = null 
+// //mouse event
+// articlesContainer.addEventListener('touchstart', function (e) {
+//     startY = e.touches[0].clientY;
+//   });
+  
+//   articlesContainer.addEventListener('touchmove', function (e) {
+//     if (startY !== null) {
+//       var currentY = e.touches[0].clientY;
+//       var deltaY = currentY - startY;
+//       articlesContainer.scrollTop -= deltaY;
+//       startY = currentY;
+//     }
+//   });
+  
+//   articlesContainer.addEventListener('touchend', function () {
+//     startY = null;
+//   });
