304279c37f6fbb3ed3bd1951f1bbbdb7e5f0b29e
SavaOurl <dendiroblek@gmail.com>
2023-10-15 14:08:49 +0400

    last preparing

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
+//swiper
+const screenWidth = window.screen.width;
+
 new Swiper('.slider__left', {
     loop: true,
-    direction: "vertical",
+    direction: (screenWidth <= 1540)? "horizontal" : "vertical",
     spaceBetween: 0,
     slidesPerView: 'auto',
     autoplay: {     
@@ -12,7 +15,7 @@ new Swiper('.slider__left', {
 });
 new Swiper('.slider__right', {
     loop: true,
-    direction: "vertical",
+    direction: (screenWidth <= 1540)? "horizontal" : "vertical",
     spaceBetween: 0,
     slidesPerView: 'auto',
     autoplay: {     
@@ -24,7 +27,7 @@ new Swiper('.slider__right', {
 });
 new Swiper('.slider__middle', {
     loop: true,
-    direction: "vertical",
+    direction: (screenWidth <= 1540)? "horizontal" : "vertical",
     spaceBetween: 0,
     slidesPerView: 'auto',
     autoplay: {     
@@ -35,3 +38,60 @@ new Swiper('.slider__middle', {
     speed: 5000,
     allowTouchMove: false,
 });
+//header full header
+const onChangeLanguage = document.querySelector('.header__options_btn-language');
+const langText = document.querySelector('.header__options_btn-language .language');
+const languages = document.querySelector('.header__options_languages');
+const chooseLang = document.querySelectorAll('.header__options_languages ul li');
+
+onChangeLanguage.addEventListener('click', () => {
+    languages.classList.add('header__options_languages-active')
+})
+chooseLang.forEach(btn => {
+    btn.addEventListener('click', () => {
+        chooseLang.forEach(btn => {
+            btn.style.backgroundColor = '#110A1D'
+        })
+        btn.style.backgroundColor = '#478BF9'
+        languages.classList.remove('header__options_languages-active');
+        langText.innerHTML = btn.getAttribute('lang');
+    })
+})
+//minimize header
+const onChangeBurgerLang = document.querySelector('.burgerMenu__language');
+const burgerLanguages = document.querySelector('.burgerMenu__language_list')
+const chooseLangBurger = document.querySelectorAll('.burgerMenu__language_list ul li');
+
+onChangeBurgerLang.addEventListener('click', () => {
+    burgerLanguages.classList.add('burgerMenu__language_list-active')
+})
+
+chooseLangBurger.forEach(btn => {
+    btn.addEventListener('click', () => {
+        chooseLangBurger.forEach(btn => {
+            btn.style.backgroundColor = '#110A1D'
+        })
+        btn.style.backgroundColor = '#478BF9'
+        burgerLanguages.classList.remove('burgerMenu__language_list-active');
+    })
+})
+//burgerMenu
+const openMenu = document.querySelector('.burgerMenu__burger');
+const burgerMenu = document.querySelector('.burgerMenu__nav');
+const burgerNavItems = document.querySelectorAll('.burgerMenu__nav_item');
+
+openMenu.addEventListener('click', () => {
+    if(burgerMenu.classList.contains('burgerMenu__nav-active')){
+        openMenu.classList.remove('burgerMenu__burger-open')
+        burgerMenu.classList.remove('burgerMenu__nav-active')
+    }else {
+        burgerMenu.classList.add('burgerMenu__nav-active')
+        openMenu.classList.add('burgerMenu__burger-open')
+    }
+})
+burgerNavItems.forEach(btn => {
+    btn.addEventListener('click', () => {
+        openMenu.classList.remove('burgerMenu__burger-open')
+        burgerMenu.classList.remove('burgerMenu__nav-active')
+    })
+})
